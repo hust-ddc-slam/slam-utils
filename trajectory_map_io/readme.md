6d987685d065206a291d6fa9470ac25d1b34edb6
larrydong <jfjxxdy2007@126.com>
2023-09-27 17:34:15 +0800

    save map

--- trajectory_map_io/readme.md
+++ trajectory_map_io/readme.md
@@ -11,6 +11,8 @@ This package save or load(TODO) trajectory & map.
 Since some slam-code publish odom (not path), and registered laser scan (not full map),  
 this code just save odometry and scan when running, and save to file when received the command.
 
+Trajectory codes can be saved to a `txt` or `csv` file, and map can be saved as `ply` or `pcd` file.
+
 Run this node:
 ```bash
 # subscribe the odom and map data.
@@ -18,4 +20,11 @@ roslaunch trajectory_saver save_trajectory.launch
 # in a new terminal type the following line to save data.
 rostopic pub /cmd std_msgs/String "s" -1   
 ```
+
+**How to modify?**
+- Modify the laser-scan's topic and odom's topic name in source codes;
+- Modify the output file path in launch file;
+- Assert laser-scan's type is `PointType` defined in source codes;
+- Modify the map downsampling size if needed;
+
 
--- trajectory_map_io/src/save_trajectory.cpp
+++ trajectory_map_io/src/save_trajectory.cpp
@@ -16,41 +16,50 @@
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 
 using namespace std;
 
-typedef pcl::PointXYZI PointType;
+typedef pcl::PointXYZ PointType;                    // modify this pcl point type, based on slam's resigtered output.
 typedef pcl::PointCloud<PointType> PointCloudType;
 
+// save trajectory and map
 int gb_save_trajectory(true), gb_save_map(true);
 std::mutex g_odom_mutex, g_map_mutex;
 
+// record lidar-scan and odoms
 std::vector<PointCloudType> gv_registered_clouds;
 std::vector<nav_msgs::Odometry> gv_odoms;
 
+// trajectory and map output file.
 std::string g_trajectory_file("/home/larrydong/trajectory.txt");
-std::string g_map_file("/home/larrydong/map.pcd");
+std::string g_map_file("/home/larrydong/map.ply");
 
+// map save downsampling size
+float g_map_ds_size(0.1);           // map downsampling size
 
+// save each lidar's scan
 void registeredCloudHandler(const sensor_msgs::PointCloud2ConstPtr &msg){
-    // TODO: 
-    // PointCloudType pc;
-	// pcl::fromROSMsg(*msg, pc);
-    // g_map_mutex.lock();
-    // gv_registered_clouds.push_back(pc);
-    // g_map_mutex.unlock();
+    PointCloudType pc;
+	pcl::fromROSMsg(*msg, pc);      // TODO: assert msg's pointcloud type is same as PointCloudType.
+    g_map_mutex.lock();
+    gv_registered_clouds.push_back(pc);
+    g_map_mutex.unlock();
 }
 
+// save each odom
 void odomHandler(const nav_msgs::Odometry::ConstPtr &odom){
     g_odom_mutex.lock();
     gv_odoms.push_back(*odom);
     g_odom_mutex.unlock();
 }
 
+// capture the "save" command
 void commandHandler(const std_msgs::String s){
     if(s.data == "s"){
         ROS_INFO("====== Save trajectory and map. ======");
-        ROS_INFO_STREAM("       save trajectory: " << gb_save_trajectory <<", save map: " << gb_save_map);
+        ROS_INFO_STREAM("       save trajectory?: " << gb_save_trajectory <<", save map?: " << gb_save_map);
+
         if(gb_save_trajectory){ 
             g_odom_mutex.lock();
             ofstream out(g_trajectory_file, ios::out);
@@ -68,7 +77,38 @@ void commandHandler(const std_msgs::String s){
                     << p.position.z << endl;
             }
             g_odom_mutex.unlock();
-            ROS_INFO_STREAM("<-- Saved " << gv_odoms.size() <<" position into file: " << g_trajectory_file);
+            ROS_INFO_STREAM("<-- Saved " << gv_odoms.size() <<" positions into file: " << g_trajectory_file);
+        }
+
+        if(gb_save_map){
+            g_map_mutex.lock();
+            ROS_INFO("--> Start to save map");
+            // merge all pointcloud and downsampling
+            PointCloudType::Ptr full_map(new PointCloudType());
+            int lidar_scan_number = gv_registered_clouds.size();
+            if(lidar_scan_number == 0){
+                ROS_ERROR("Empty lidar-scan and no map saved");
+                return ;
+            }
+            
+            for(auto pc:gv_registered_clouds)      // merge all points (from each scan)
+                *full_map += pc;
+            
+            pcl::VoxelGrid<PointType> dsFilter;
+            dsFilter.setLeafSize(g_map_ds_size, g_map_ds_size, g_map_ds_size);
+            dsFilter.setInputCloud(full_map);
+            dsFilter.filter(*full_map);
+            g_map_mutex.unlock();
+
+            // save to ply/pcd file. Automatically check the file format and save.
+            string map_file_type = g_map_file.substr(g_map_file.size() - 3, 3); // get output file type.
+            if(map_file_type == "ply")
+                pcl::io::savePLYFile(g_map_file, *full_map);
+            else if(map_file_type == "pcd")
+                pcl::io::savePCDFile(g_map_file, *full_map);
+            else
+                ROS_ERROR_STREAM("Unexpected map save format.");
+            ROS_INFO_STREAM("--> Got " << lidar_scan_number<<" lidar-scan and saved " << full_map->size() << " points into file: " << g_map_file);
         }
     }
     else{
@@ -89,12 +129,14 @@ int main(int argc, char **argv) {
 
     nh.getParam("trajectory_save_file", g_trajectory_file);
     nh.getParam("map_save_file", g_map_file);
+    nh.getParam("map_downsample_size", g_map_ds_size);
 
     // start to save map/trajectory
     // rostopic pub /cmd std_msgs/String "s" -1
     ros::Subscriber subString = nh.subscribe<std_msgs::String>("/cmd", 1, commandHandler);
+    // TODO: modify the topic name according to your slam algorithm
     ros::Subscriber subOdometry = nh.subscribe<nav_msgs::Odometry>("/Odometry", 100, odomHandler);
-    // ros::Subscriber subMap = nh.subscribe<PointCloudType>("/map", 100, registeredCloudHandler);
+    ros::Subscriber subMap = nh.subscribe<sensor_msgs::PointCloud2>("/cloud_registered", 100, registeredCloudHandler);
     ros::Rate r(100);
     while (ros::ok()) {
         ros::spinOnce();
